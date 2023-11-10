0a477a6731b123eda29e7451cfaab6004687364f
Sheldon Strebe <10855240@uvu.edu>
2023-11-10 14:06:15 -0700

    added a spinner

--- tutor-visits-convert.py
+++ tutor-visits-convert.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 import pytz
 import test
+import spinner
 
 def addDash(s):
     return s[:4] + '-' + s[4:6] + '-' + s[6:]
@@ -30,11 +31,11 @@ def main():
     outputfile = ''
 
     if sys.argv[1] == '-t' and len(sys.argv) != 3:
-        print('Usage: python fts.py -t <testfile>')
+        print('Usage: python tutor-visits-convert.py -t <testfile>')
         sys.exit(1)
     
     if sys.argv[1] == '-o' and len(sys.argv) != 4:
-        print('Usage: python fts.py <inputfile> <outputfile>')
+        print('Usage: python tutor-visits-convert.py <inputfile> <outputfile>')
         sys.exit(1)
 
     inputfile = sys.argv[2]
@@ -47,67 +48,81 @@ def main():
     df = pd.read_csv(inputfile)
     df.columns = index
 
-    # convert the date column to YYYY-MM-DD format then convert to UTC in seconds since epoch
-    # df['date'] = df['date'].map(lambda x: addDash(str(x)))
-    df['date'] = df['date'].map(lambda x: toUTC(str(x)))
+    with spinner.Spinner("converting data... "):
+        # convert the date column to YYYY-MM-DD format then convert to UTC in seconds since epoch
+        # df['date'] = df['date'].map(lambda x: addDash(str(x)))
+        df['date'] = df['date'].map(lambda x: toUTC(str(x)))
 
-    # convert the timei column to 24 hour format
-    df['timei'] = df['timei'].map(lambda x: str(x)[2:])
-    df['timei'] = df['timei'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
+        # convert the timei column to 24 hour format
+        df['timei'] = df['timei'].map(lambda x: str(x)[2:])
+        df['timei'] = df['timei'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
 
-    # convert the timei column to 24 hour format
-    df['timeo'] = df['timeo'].map(lambda x: str(x)[2:])
-    df['timeo'] = df['timeo'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
+        # convert the timei column to 24 hour format
+        df['timeo'] = df['timeo'].map(lambda x: str(x)[2:])
+        df['timeo'] = df['timeo'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
 
-    # convert the timev column to HH:MM format
-    df['timev'] = df['timev'].map(lambda x: str(x)[2:])
-    df['timev'] = df['timev'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
+        # convert the timev column to HH:MM format
+        df['timev'] = df['timev'].map(lambda x: str(x)[2:])
+        df['timev'] = df['timev'].map(lambda x: x[:2] + ':' + x[2:] if len(str(x)) == 4 else x[:2] + ':' + x[2:] + '0' if len(str(x)) == 3 else x[:2] + ':' + x[2:] + '0' + '0' if len(str(x)) == 2 else x[:1] + '0:00')
 
     #test the data
     errNum = 0
     rowList = []
-    for index, row in df.iterrows():
-        if not test.test_date(str(row['date'])):
-            print('Error on row ' + str(index) + ' date value ' + str(row['date']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_times(str(row['timei'])):
-            print('Error on row ' + str(index) + ' timei value ' + str(row['timei']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_times(str(row['timeo'])):
-            print('Error on row ' + str(index) + ' timeo value ' + str(row['timeo']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_times(str(row['timev'])):
-            print('Error on row ' + str(index) + ' timev value ' + str(row['timev']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_uvid(str(row['uvid'])):
-            print('Error on row ' + str(index) + ' uvid value ' + str(row['uvid']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_name(str(row['name'])):
-            print('Error on row ' + str(index) + ' name value ' + str(row['name']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_center(str(row['center'])):
-            print('Error on row ' + str(index) + ' center value ' + str(row['center']))
-            rowList.append(index)
-            errNum += 1
-        if not test.test_class(str(row['class'])):
-            print('Error on row ' + str(index) + ' class value ' + str(row['class']))
-            rowList.append(index)
-            errNum += 1
+    errors = []
+    with spinner.Spinner("testing data conversions... "):
+        for index, row in df.iterrows():
+            if not test.test_date(str(row['date'])):
+                errors.append('Error on row ' + str(index) + ' date value ' + str(row['date']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_times(str(row['timei'])):
+                errors.append('Error on row ' + str(index) + ' timei value ' + str(row['timei']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_times(str(row['timeo'])):
+                errors.append('Error on row ' + str(index) + ' timeo value ' + str(row['timeo']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_times(str(row['timev'])):
+                errors.append('Error on row ' + str(index) + ' timev value ' + str(row['timev']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_uvid(str(row['uvid'])):
+                errors.append('Error on row ' + str(index) + ' uvid value ' + str(row['uvid']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_name(str(row['name'])):
+                errors.append('Error on row ' + str(index) + ' name value ' + str(row['name']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_center(str(row['center'])):
+                errors.append('Error on row ' + str(index) + ' center value ' + str(row['center']))
+                rowList.append(index)
+                errNum += 1
+            if not test.test_class(str(row['class'])):
+                errors.append('Error on row ' + str(index) + ' class value ' + str(row['class']))
+                rowList.append(index)
+                errNum += 1
 
     # write the dataframe to a csv file if there are no errors
+
     if errNum == 0:
+        print('No errors found. Writing converted data to ' + outputfile)
         df.to_csv(outputfile, index=False)
     else:
-        i = input('There were ' + str(errNum) + ' errors. would you like to remove them? (y/n)')
-        df.drop(df.index[rowList], inplace=True) if i == 'y' else print('No changes made.')
-        df.to_csv(outputfile, index=False) if i == 'y' else print('No output file created.')
-        print('Finished')
+        for e in errors:
+            print(e)
+        print('There were ' + str(errNum) + ' errors. Would you like to remove them? (y/n)')
+        i = input()
+        if i == 'y':
+            df.drop(df.index[rowList], inplace=True)
+            df.to_csv(outputfile, index=False)
+            print('Items removed. Writing converted data to ' + outputfile)
+        else:
+            df.to_csv(outputfile, index=False)
+            print('No changes made. Writing converted data to ' + outputfile + ' with errors')
+
+    print('finished')
 
 
 
